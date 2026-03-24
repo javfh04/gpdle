55d3745f7df8d0526a573ce9eab9967b39a2dee5
Javier <freirejavier158@gmail.com>
2026-03-24 19:57:16 +0100

    Fix definitivo de db.sql

--- database/db.sql
+++ database/db.sql
@@ -4,10 +4,10 @@ USE Pilotos;
 
 -- Pilotos
 CREATE TABLE Pilotos (
-    idPiloto INT(100) PRIMARY KEY AUTO_INCREMENT,
+    idPiloto INT PRIMARY KEY AUTO_INCREMENT,
     Nombre VARCHAR(50) NOT NULL,
-    Dorsal INT(100) NOT NULL,
+    Dorsal INT NOT NULL,
     Equipo VARCHAR(100) NOT NULL, 
     Marca VARCHAR(50) NOT NULL,    
-    Categoría VARCHAR(50) NOT NULL
+    Categoria VARCHAR(50) NOT NULL
 );
