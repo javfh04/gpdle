fa01d8f3b4b509738bc00bef0ee6becbf529ff18
Javier <freirejavier158@gmail.com>
2026-03-24 19:26:56 +0100

    Introduce creación de tablas

---
+++ database/db.sql
@@ -0,0 +1,13 @@
+-- Database
+CREATE DATABASE Pilotos;
+USE Pilotos;
+
+-- Pilotos
+CREATE TABLE Pilotos (
+    idPiloto INT(100), PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(50) NOT NULL,
+    Dorsal INT(100) NOT NULL,
+    Equipo VARCHAR(100) NOT NULL, 
+    Marca VARCHAR(50) NOT NULL,    
+    Categoría VARCHAR(50) NOT NULL
+);
